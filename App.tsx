6e118144234323970b1eacfe96b38166ba3a5126
MateoJM2003 <mateoldu2003@gmail.com>
2024-08-01 11:27:22 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomeWorldScreen } from './src/screens/HomeWorldScreen';
+import { CounterScreen } from './src/screens/CounterScreen';
+
+const App = () => {
+  return (
+    //<HomeWorldScreen/>
+    <CounterScreen/>
+  )
+}
+
+export default App;
